c4c7d0040eae81bfd3e64772c4f42cf6ad6650b5
TheoLeanse <theoleanse@gmail.com>
2016-04-18 16:51:02 +0100

    don't send subscribers away from the article page

--- src/light-signup.js
+++ src/light-signup.js
@@ -18,7 +18,7 @@ export default {
 			'INVALID_REQUEST': 'Sorry, something went wrong. Please try again.',
 			'ALREADY_SUBSCRIBED': 'It looks like you’re currently receiving the daily top stories summary email. If you’re interested in getting access to more FT content, why not <a target="_blank" style="text-decoration:none;color:#27757B;" href="/products">sign up to a 4 week Trial</a>?',
 			'USER_ARCHIVED': 'It looks like you’ve signed up to the daily top stories summary email before. If you’re interested in getting access to more FT content, why not <a target="_blank" style="text-decoration:none;color:#27757B;" href="/products">sign up to a 4 week Trial</a>?',
-			'USER_NOT_ANONYMOUS': `It looks like you already have an account with us. <a target="_blank" href="/login?location=${pageLocation}" style="text-decoration:none;color:#27757B;">Sign in</a>.`
+			'USER_NOT_ANONYMOUS': `It looks like you already have an account with us. <a href="/login?location=${pageLocation}" style="text-decoration:none;color:#27757B;">Sign in</a>.`
 		};
 
 		// Handle user interaction
