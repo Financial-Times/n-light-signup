239620505b44214890eced734f7b6aa67f9771aa
Matt Brennan <mattyb1000@gmail.com>
2016-04-25 16:34:35 +0100

    get demo working

---
+++ demos/src/demo.js
@@ -0,0 +1,3 @@
+import oLightSignup from '../../';
+
+oLightSignup.init();
--- demos/src/helpers/flags.js
+++
@@ -1,3 +0,0 @@
-window.nextFeatureFlags = [
-	{"name":"cookieMessage","safeState":true,"expiry":"2016-11-30T00:00:00.000Z","state":true}
-];
