2f0e87f13c134e209a3faa3661d98cf6025d8d33
Matt Brennan <mattyb1000@gmail.com>
2016-05-20 11:09:42 +0100

    Revert "extract responsemsg stuff so it can be used externally"

--- src/email-only-signup.js
+++ src/email-only-signup.js
@@ -5,94 +5,95 @@ const defaultOptions = {
 	signupUrl: '/signup/api/light-signup'
 };
 
-export function getResponseMsg(response, pageLocation) {
-	// Keep marketing copy somewhere
-	const responseMsg = {
-		'SUBSCRIPTION_SUCCESSFUL': 'Thanks – look out for your first briefing soon.',
-		'INVALID_REQUEST': 'Sorry, something went wrong. Please try again.',
-		'ALREADY_SUBSCRIBED': 'It looks like you’re currently receiving the daily top stories summary email. If you’re interested in getting access to more FT content, why not <a target="_blank" style="text-decoration:none;color:#27757B;" href="/products?segID=0801043">sign up to a 4 week Trial</a>?',
-		'USER_ARCHIVED': 'It looks like you’ve signed up to the daily top stories summary email before. If you’re interested in getting access to more FT content, why not <a target="_blank" style="text-decoration:none;color:#27757B;" href="/products?segID=0801043">sign up to a 4 week Trial</a>?',
-		'USER_NOT_ANONYMOUS': `It looks like you already have an account with us. <a href="/login?location=${pageLocation}" style="text-decoration:none;color:#27757B;">Sign in</a>.`
-	};
+export default {
 
-	return responseMsg[response] ? responseMsg[response] : responseMsg.INVALID_REQUEST;
-}
+	init(el, options = {}) {
+		defaultsDeep(options, optionsFromData(el), defaultOptions);
 
-export function init(el, options = {}) {
-	defaultsDeep(options, optionsFromData(el), defaultOptions);
+		const closeButton = el.querySelector('[data-o-email-only-signup-close]');
+		const lightSignupForm = el.querySelector('[data-o-email-only-signup-form]');
+		const displaySection = el.querySelector('[data-o-email-only-signup-completion-message]');
+		const emailField = el.querySelector('input[name=email]');
+		const invalidEmailMessage = el.querySelector('[data-o-email-only-signup-email-error]');
 
-	const closeButton = el.querySelector('[data-o-email-only-signup-close]');
-	const lightSignupForm = el.querySelector('[data-o-email-only-signup-form]');
-	const displaySection = el.querySelector('[data-o-email-only-signup-completion-message]');
-	const emailField = el.querySelector('input[name=email]');
-	const invalidEmailMessage = el.querySelector('[data-o-email-only-signup-email-error]');
+		// Keep marketing copy somewhere
 
-	const pageLocation = window.location.href;
+		const pageLocation = window.location.href;
 
-	// Handle user interaction
-	lightSignupForm.addEventListener('submit', (e) => {
-		e.preventDefault();
+		const responseMsg = {
+			'SUBSCRIPTION_SUCCESSFUL': 'Thanks – look out for your first briefing soon.',
+			'INVALID_REQUEST': 'Sorry, something went wrong. Please try again.',
+			'ALREADY_SUBSCRIBED': 'It looks like you’re currently receiving the daily top stories summary email. If you’re interested in getting access to more FT content, why not <a target="_blank" style="text-decoration:none;color:#27757B;" href="/products?segID=0801043">sign up to a 4 week Trial</a>?',
+			'USER_ARCHIVED': 'It looks like you’ve signed up to the daily top stories summary email before. If you’re interested in getting access to more FT content, why not <a target="_blank" style="text-decoration:none;color:#27757B;" href="/products?segID=0801043">sign up to a 4 week Trial</a>?',
+			'USER_NOT_ANONYMOUS': `It looks like you already have an account with us. <a href="/login?location=${pageLocation}" style="text-decoration:none;color:#27757B;">Sign in</a>.`
+		};
 
-		const email = emailField.value;
+		// Handle user interaction
 
-		if (isValidEmail(email)) {
-			const opts = {
-				method: 'POST',
-				headers: {
-					'Content-type': 'application/x-www-form-urlencoded'
-				},
-				credentials: 'include',
-				body: `email=${formatEmail(email)}`
-			};
+		lightSignupForm.addEventListener('submit', (e) => {
+			e.preventDefault();
 
-			fetch(options.signupUrl, opts)
-			.then(response => response.text())
-			.then(response => {
-				displaySection.innerHTML = getResponseMsg(response, pageLocation);
-			})
-			.catch(err => console.log(err));
+			const email = emailField.value;
 
-		} else {
-			toggleValidationErrors();
-		}
-	});
+			if (isValidEmail(email)) {
+				const opts = {
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/x-www-form-urlencoded'
+					},
+					credentials: 'include',
+					body: `email=${formatEmail(email)}`
+				};
 
-	closeButton.addEventListener('click', () => {
-		el.style.display = 'none';
-		el.setAttribute('aria-hidden', true);
-	});
+				fetch(options.signupUrl, opts)
+					.then(response => response.text())
+					.then(response => {
+						displaySection.innerHTML = responseMsg[response] ? responseMsg[response] : responseMsg["INVALID_REQUEST"];
+					})
+					.catch(err => console.log(err));
 
-	emailField.addEventListener('click', () => {
-		if (lightSignupForm.classList.contains('o-forms--error')) {
-			toggleValidationErrors();
-		}
-	});
-
-	// Validation helpers
-
-	function isValidEmail(email) {
-		return /(.+)@(.+)/.test(email);
-	}
-
-	function toggleValidationErrors() {
-		lightSignupForm.classList.toggle('o-forms--error');
-		invalidEmailMessage.classList.toggle('o-email-only-signup__visually-hidden');
-	}
-
-	function formatEmail(email) {
-		return encodeURIComponent(email.trim()).replace('%20', '+');
-	}
-
-	function optionsFromData(el) {
-		const options = {};
-		Object.keys(defaultOptions).forEach(key => {
-			// convert optionKeyLikeThis to data-o-email-only-signup-option-key-like-this
-			const attr = 'data-o-email-only-signup-' + kebabCase(key);
-			if(el.hasAttribute(attr)) {
-				options[key] = el.getAttribute(attr);
+			} else {
+				toggleValidationErrors();
 			}
 		});
 
-		return options;
+		closeButton.addEventListener('click', () => {
+			el.style.display = 'none';
+			el.setAttribute('aria-hidden', true);
+		});
+
+		emailField.addEventListener('click', () => {
+			if (lightSignupForm.classList.contains('o-forms--error')) {
+				toggleValidationErrors();
+			}
+		});
+
+		// Validation helpers
+
+		function isValidEmail(email) {
+			return /(.+)@(.+)/.test(email);
+		}
+
+		function toggleValidationErrors() {
+			lightSignupForm.classList.toggle('o-forms--error');
+			invalidEmailMessage.classList.toggle('o-email-only-signup__visually-hidden');
+		}
+
+		function formatEmail(email) {
+			return encodeURIComponent(email.trim()).replace('%20', '+');
+		}
+
+		function optionsFromData(el) {
+			const options = {};
+			Object.keys(defaultOptions).forEach(key => {
+				// convert optionKeyLikeThis to data-o-email-only-signup-option-key-like-this
+				const attr = 'data-o-email-only-signup-' + kebabCase(key);
+				if(el.hasAttribute(attr)) {
+					options[key] = el.getAttribute(attr);
+				}
+			});
+
+			return options;
+		}
 	}
 };
